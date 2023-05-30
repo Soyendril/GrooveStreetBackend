c92d4c4a6124abb69a1effbdad585e8b7b057df6
Titouan Andritsos <titouan.andritsos@hotmail.fr>
2023-05-30 11:07:07 +0200

    first commit with dependencies

---
+++ src/test/java/com/GrooveSpring/GrooveSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GrooveSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrooveSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
